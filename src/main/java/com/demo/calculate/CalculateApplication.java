2cc1a1750613438b703f4949855a304b8c77fcb0
wb_wujianwei <wb_wujianwei@youzan.com>
2021-05-08 18:16:10 +0800

    第一次提交

---
+++ src/main/java/com/demo/calculate/CalculateApplication.java
@@ -0,0 +1,13 @@
+package com.demo.calculate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalculateApplication.class, args);
+    }
+
+}
